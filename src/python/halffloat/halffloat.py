4b0e6f83d1e88cc8b502ec19be1170b3fd39e00a
LI JI <146397706+lji-ilm@users.noreply.github.com>
2024-07-29 18:30:07 +0000

    try codespace edit halffloat class

--- src/python/halffloat/halffloat.py
+++ src/python/halffloat/halffloat.py
@@ -12,32 +12,38 @@ class halffloat:
 
     def __init__(self, v, LITTLE_ENDIAN=True):
         assert type(v) == float16
-        self._updateValue(v)
+        self._LITTLE_ENDIAN = LITTLE_ENDIAN
+        self._updateValue(v)        
 
     def _updateValue(self, v):
         self._v = v
         self._bytes = v.tobytes()
         self._hex = self._bytes.hex().upper()
-        self._bits = BitArray()
+        self._bits = BitArray(self._bytes)
 
-        if LITTLE_ENDIAN:
+        if self._LITTLE_ENDIAN:
             self._lo = self._bits[:8]
+            self._lostr = self._bits.bin[:8]
             self._hi = self._bits[8:]
+            self._histr = self._bits.bin[8:]
         else:
             self._hi = self._bits[:8]
+            self._histr = self._bits.bin[:8]
             self._lo = self._bits[8:]
+            self._lostr = self._bits.bin[8:]
 
     def __str__(self):
         hexstr = "0x" + self._hex
-        return (str(v) 
+        return (str(self._v)
+                + " " 
                 + hexstr 
                 + "|"
                 + self.CCYAN
-                + self._hi[0]
+                + self._histr[0]
                 + self.CBLUE
-                + self._hi[1:6]
+                + self._histr[1:6]
                 + self.CRED
-                + self._hi[6:]
+                + self._histr[6:]
                 + " "
-                + self._lo
+                + self._lostr
                 + self.CEND)
