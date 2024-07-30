22223d129331f25ceb36118c70e1b90865640bf9
LI JI <146397706+lji-ilm@users.noreply.github.com>
2024-07-30 20:29:18 +0000

    add components calculations

--- src/python/halffloat/halffloat.py
+++ src/python/halffloat/halffloat.py
@@ -1,5 +1,5 @@
 from numpy import float16
-from bitstring import BitArray
+from bitstring import Bits, BitArray
 
 class halffloat:
     CBACK = "\033[40m"
@@ -19,7 +19,7 @@ class halffloat:
         self._v = v
         self._bytes = v.tobytes()
         self._hex = self._bytes.hex().upper()
-        self._bits = BitArray(self._bytes)
+        self._bits = Bits(self._bytes)
 
         if self._LITTLE_ENDIAN:
             self._lo = self._bits[:8]
@@ -32,7 +32,48 @@ class halffloat:
             self._lo = self._bits[8:]
             self._lostr = self._bits.bin[8:]
 
-    def __str__(self):
+    @property
+    def negative(self):
+        return self._hi[0]
+    
+    @property
+    def exponent(self):
+        return self._hi[1:6].unpack("uint:5")[0]
+    
+    def _unpack_binary_fraction(self, b, length):
+        current_frac = 0.5
+        step = 0.5
+        sum = 0
+        for i in range(length):
+            if b[i]:
+                sum = sum + current_frac
+            current_frac = current_frac * step
+        return sum
+        
+    @property
+    def mantissa(self):
+        m = BitArray(self._lo)
+        m.prepend(self._hi[6:8])
+        return self._unpack_binary_fraction(m, len(m))
+
+    @property
+    def significand(self):
+        return self.mantissa + 1
+    
+    @property
+    def power(self):
+        return self.exponent - 15
+    
+    @property
+    def formula(self):
+        fo = f"2^{self.power}*(1+{self.significand})"
+        if self.negative:
+            fo = "-1*" + fo
+        fo = f"{self._v} = " + fo
+        return fo
+
+    @property
+    def hex_and_bits(self):
         hexstr = "0x" + self._hex
         return (str(self._v)
                 + " " 
@@ -47,3 +88,6 @@ class halffloat:
                 + " "
                 + self._lostr
                 + self.CEND)
+
+    def __str__(self):
+        return self.hex_and_bits
